b702d22eca95f250d344962e38aa051401e6deb0
=epogrebnyak <e.pogrebnyak@gmail.com>
2016-03-19 15:04:17 +0300

    refactor, tests and mbk/mkr problem

--- cbr_soap.py
+++ cbr_soap.py
@@ -5,23 +5,23 @@
 # WSDL нужен чтобы обьявить приложению какая структура будет потребляться вэбсервисом
 # вэбсервис - это обычный xml слушатель и отправитель также в формате XML
 # следовательно результат можно распарсить и получить XML
-# для понимания работы с вэбсервисом - приложение SoapUI
+# для понимания работы с вэбсервисом - приложение SoapUI - <https://www.soapui.org/>
 
 # --- Requirement ---
 # pysimplesoap installed from git repo by:
 # pip install -e git+git@github.com:pysimplesoap/pysimplesoap.git@07ab7217ccc2572d40ad36c73867fc9be8fe2794#egg=soap2py-master
-# 
+
 import pickle
+import requests
+import os
 
 from collections import OrderedDict
 from datetime import datetime, timedelta
-from os.path import exists
-
+import pandas as pd
+    
 from bs4 import BeautifulSoup
 from dateutil.parser import parse as parse_dt
 from pysimplesoap.client import SoapClient
-
-import requests
 
 
 class SourceAddress():
@@ -54,7 +54,7 @@ class DataSOAP(SourceAddress):
             self.wsdl_info = pickle.load(f)
 
     def __init__(self):
-        if exists(self.cache_file_name):
+        if os.path.exists(self.cache_file_name):
             self.load_local()
         else:
             self.update()
@@ -151,7 +151,7 @@ def yield_ruonia(start, end):
         # intent: this flat data goes to sqlite later
 
 
-def yield_valutes():
+def yield_currencies():
     response = call_cbr('EnumValutes', False)
     for x in response.find_all('enumvalutes'):
         result = {
@@ -168,22 +168,28 @@ def yield_valutes():
 def yield_curs(start, end, code):
     response = call_cbr('GetCursDynamic', start, end, code)
     for x in response.find_all('valutecursdynamic'):
-        yield {'date': parse_dt(x.cursdate.text), 'curs': float(x.vcurs.text)}
+        yield as_dict('USDRUR_CBR', parse_dt(x.cursdate.text), float(x.vcurs.text))
 
 
 def yield_mkr(start, end):
     response = call_cbr('MKR', start, end)
     for x in response.find_all('mkr'):
-        yield {
-            'date': parse_dt(x.cdate.text),
-            'p1': float(x.p1.text),
-            'd1': float(x.d1.text) if x.d1 else None,
-            'd7': float(x.d7.text) if x.d7 else None,
-            'd30': float(x.d30.text) if x.d30 else None,
-            'd90': float(x.d90.text) if x.d90 else None,
-            'd180': float(x.d180.text) if x.d180 else None,
-            'd360': float(x.d360.text) if x.d360 else None,
-        }
+        dt = parse_dt(x.cdate.text)
+        yield as_dict('mbk_p1', dt, float(x.p1.text))
+        yield as_dict('mbk_d1', dt, float(x.d1.text) if x.d1 else None)
+        
+        # new todo-1: must yield flat stream as above, complete fucntions
+         
+        # {
+            # 'date': parse_dt(x.cdate.text),
+            # 'p1': float(x.p1.text),
+            # 'd1': float(x.d1.text) if x.d1 else None,
+            # 'd7': float(x.d7.text) if x.d7 else None,
+            # 'd30': float(x.d30.text) if x.d30 else None,
+            # 'd90': float(x.d90.text) if x.d90 else None,
+            # 'd180': float(x.d180.text) if x.d180 else None,
+            # 'd360': float(x.d360.text) if x.d360 else None,
+        # }
 
 
 # todo: try write one or several other yield functions 
@@ -193,109 +199,66 @@ def yield_mkr(start, end):
 #      доллар - R01235
 #      евро - R01239
 
+def make_df(gen):
+    df = pd.DataFrame(gen)
+    df = df.pivot(columns='name', values='value', index='date')
+    df.index = pd.to_datetime(df.index)
+    return df 
 
-def main():
-    # todo: make proper assert to check get_operation_parameters output, maybe several asserts 
-    # assert DataSOAP().get_operation_parameters('Ruonia') == {'fromDate': <class 'datetime.datetime'>, 'ToDate': <class 'datetime.datetime'>}
- 
-    #start = datetime.now() - timedelta(days=30)
-    #end = datetime.now()
+    
+def yield_usd(start, end):
+    currency_code = 'R01235'
+    return yield_curs(start, end, currency_code)
+    
+def yield_eur(start, end):
+    currency_code = 'R01239'
+    return yield_curs(start, end, currency_code)    
+    
+def get_online_df(operation, start, end):
+    functions = {'ruonia':yield_ruonia, "usdrur": yield_usd, "eurrur":yield_eur, 'mkr':yield_mkr}
+    gen = functions[operation](start, end)
+    return make_df(gen)
+
+class Interval():
+    """Start and end dates for data import based on incomplete inputs"""
+    # start, end = Interval(start, end).range
+    def __init__(self, start=None, end=None):
+        self.range = None, None 
+        if start is not None and end is not None:  
+            self.range = start, end       
+        elif end is None:
+            end = datetime.now()
+            if start is None:
+                start = datetime.now() - timedelta(days=15)
+        # not todo: check start, end class 
+        #           check start > end
+        #           may be a function, not a class
+    
+if __name__ == "__main__":
+    # note: script section - code from below either goes to test or to functions above
+        
     start = datetime(2016, 3, 13)
     end = datetime(2016, 3, 15)
-    assert make_xml_parameter_string('Ruonia', start, end) == '<web:fromDate>2016-03-13</web:fromDate><web:ToDate>2016-03-15</web:ToDate>'
 
-    response = call_cbr('Ruonia', start, end)
-    print(type(response))
+    # currency codes
+    # 'currencies.csv' has useful codes like EUR, USD - may use for column names in dataframe
+    currencies = pd.DataFrame(yield_currencies())
+    currencies.index = currencies.name
+    currencies.to_csv('_currencies_info.csv')
+    
+    # rur/usd exhange rate
+    usd_rate = get_online_df("usdrur", start, end)
+    usd_rate.to_csv('_usdrur.csv')
 
-    assert(isinstance(response, BeautifulSoup))
-
-    reference = BeautifulSoup(reference_response_string.strip(), 'lxml')
-    assert(reference.prettify() == response.prettify())
-
-    assert list(yield_ruonia(start, end)) == [as_dict('ruonia_rate', '2016-03-14',  11.07), 
-                                              as_dict('ruonia_vol',  '2016-03-14', 150.46), 
-                                              as_dict('ruonia_rate', '2016-03-15',  11.1 ), 
-                                              as_dict('ruonia_vol',  '2016-03-15', 185.87)]
-                                              
-    # todo: correct if something fails below
-    import pandas as pd
-
-    valutes = pd.DataFrame(yield_valutes())
-    valutes.index = valutes.name
-    valutes.to_csv('valutes.csv')
-
-    usd_rate = pd.DataFrame(yield_curs(start, end, 'R01235'))
-    usd_rate.index = usd_rate.date
-    usd_rate.to_csv('usd_rate.csv')
-
-    mkr = pd.DataFrame(yield_mkr(start, end))
-    mkr.index = mkr.date
-    mkr.to_csv('mkr.csv')
-
-    def make_df(gen):
-        df = pd.DataFrame(gen)
-        df = df.pivot(columns='name', values='value', index='date')
-        df.index = pd.to_datetime(df.index)
-        return df      
+    # rur/usd exhange rate
+    eur_rate = get_online_df("eurrur", start, end)
+    eur_rate.to_csv('_eurrur.csv')
+    
+    # mkr = Ставки межбанковского кредитного рынка
+    print(list(yield_mkr(start, end)))
+    #mkr = get_online_df("mkr", start, end)
+    #mkr.to_csv('_mkr.csv')    
    
-    ruonia_df = make_df(yield_ruonia(start, end))
-    ruonia_df.to_csv('ruonia.txt')
-    print(ruonia_df.to_csv())
-        
-    # df = pd.DataFrame(yield_ruonia(start, end))
-    ## reshape/pivot using date/value
-    # df = df.pivot(columns='name', values='value', index='date')
-    # df.index = pd.to_datetime(df.index)
-
-            
-# SOAP UI response to *cbr_body*
-reference_response_string = """
-<?xml version="1.0" encoding="utf-8"?>
-<html>
-<body>
-<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-    <soap:Body>
-        <RuoniaResponse xmlns="http://web.cbr.ru/">
-            <RuoniaResult>
-                <xs:schema id="Ruonia" xmlns="" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:msdata="urn:schemas-microsoft-com:xml-msdata">
-                <xs:element name="Ruonia" msdata:IsDataSet="true" msdata:UseCurrentLocale="true">
-                    <xs:complexType>
-                        <xs:choice minOccurs="0" maxOccurs="unbounded">
-                            <xs:element name="ro">
-                            <xs:complexType>
-                                <xs:sequence>
-                                    <xs:element name="D0" msdata:Caption="Дата" type="xs:dateTime" minOccurs="0"/>
-                                    <xs:element name="ruo" msdata:Caption="Ставка, %" type="xs:decimal" minOccurs="0"/>
-                                    <xs:element name="vol" msdata:Caption="Объем сделок,по которым произведен расчет ставки RUONIA, млрд. руб." type="xs:decimal" minOccurs="0"/>
-                                </xs:sequence>
-                            </xs:complexType>
-                            </xs:element>
-                        </xs:choice>
-                    </xs:complexType>
-                </xs:element>
-                </xs:schema>
-                <diffgr:diffgram xmlns:msdata="urn:schemas-microsoft-com:xml-msdata" xmlns:diffgr="urn:schemas-microsoft-com:xml-diffgram-v1">
-                <Ruonia xmlns="">
-                    <ro diffgr:id="ro1" msdata:rowOrder="0">
-                        <D0>2016-03-14T00:00:00+03:00</D0>
-                        <ruo>11.0700</ruo>
-                        <vol>150.4600</vol>
-                    </ro>
-                    <ro diffgr:id="ro2" msdata:rowOrder="1">
-                        <D0>2016-03-15T00:00:00+03:00</D0>
-                        <ruo>11.1000</ruo>
-                        <vol>185.8700</vol>
-                    </ro>
-                </Ruonia>
-                </diffgr:diffgram>
-            </RuoniaResult>
-        </RuoniaResponse>
-    </soap:Body>
-</soap:Envelope>
-</body>
-</html>
-"""
-
-
-if __name__ == "__main__":
-    main()
+    # ставка ruonia
+    ruonia_df = get_online_df("ruonia", start, end) # make_df(yield_ruonia(start, end))
+    ruonia_df.to_csv('_ruonia.txt')
---
+++ test_.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+from bs4 import BeautifulSoup
+import cbr_soap
+
+SAMPLE_DATE_START = datetime(2016, 3, 13)
+SAMPLE_DATE_END = datetime(2016, 3, 15)
+
+def test_param_string():
+    WEB_1 = '<web:fromDate>2016-03-13</web:fromDate><web:ToDate>2016-03-15</web:ToDate>'
+    assert WEB_1 == cbr_soap.make_xml_parameter_string('Ruonia', SAMPLE_DATE_START, SAMPLE_DATE_END)
+
+def test_cbr_call(): 
+    response = cbr_soap.call_cbr('Ruonia', SAMPLE_DATE_START, SAMPLE_DATE_END)
+    assert(isinstance(response, BeautifulSoup))
+
+    reference = BeautifulSoup(reference_ruonia_xml_string().strip(), 'lxml')
+    assert(reference.prettify() == response.prettify())
+
+def test_yield_ruonia():
+    RUONIA_SAMPLE = [cbr_soap.as_dict(*x) for x in [('ruonia_rate', '2016-03-14',  11.07), 
+                                                    ('ruonia_vol',  '2016-03-14', 150.46), 
+                                                    ('ruonia_rate', '2016-03-15',  11.10), 
+                                                    ('ruonia_vol',  '2016-03-15', 185.87)]
+                                           ]                                           
+    assert RUONIA_SAMPLE == list(cbr_soap.yield_ruonia(start = SAMPLE_DATE_START, end = SAMPLE_DATE_END))
+
+def test_ruonia_df():
+    ruonia_df = cbr_soap.get_online_df("ruonia", SAMPLE_DATE_START, SAMPLE_DATE_END) # make_df(yield_ruonia(start, end))
+    SAMPLE_CSV_REPR = "'date,ruonia_rate,ruonia_vol\\n2016-03-14,11.07,150.46\\n2016-03-15,11.1,185.87\\n'"
+    assert SAMPLE_CSV_REPR == ruonia_df.to_csv().__repr__()     
+
+
+def test_Parameters():
+    # todo: make proper assert to check get_operation_parameters output, maybe several asserts 
+    # assert cbr_soap.Parameters('Ruonia') == {'fromDate': <class 'datetime.datetime'>, 'ToDate': <class 'datetime.datetime'>}
+    pass
+    
+def reference_ruonia_xml_string():
+    return """<?xml version="1.0" encoding="utf-8"?>
+<html>
+<body>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <soap:Body>
+        <RuoniaResponse xmlns="http://web.cbr.ru/">
+            <RuoniaResult>
+                <xs:schema id="Ruonia" xmlns="" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:msdata="urn:schemas-microsoft-com:xml-msdata">
+                <xs:element name="Ruonia" msdata:IsDataSet="true" msdata:UseCurrentLocale="true">
+                    <xs:complexType>
+                        <xs:choice minOccurs="0" maxOccurs="unbounded">
+                            <xs:element name="ro">
+                            <xs:complexType>
+                                <xs:sequence>
+                                    <xs:element name="D0" msdata:Caption="Дата" type="xs:dateTime" minOccurs="0"/>
+                                    <xs:element name="ruo" msdata:Caption="Ставка, %" type="xs:decimal" minOccurs="0"/>
+                                    <xs:element name="vol" msdata:Caption="Объем сделок,по которым произведен расчет ставки RUONIA, млрд. руб." type="xs:decimal" minOccurs="0"/>
+                                </xs:sequence>
+                            </xs:complexType>
+                            </xs:element>
+                        </xs:choice>
+                    </xs:complexType>
+                </xs:element>
+                </xs:schema>
+                <diffgr:diffgram xmlns:msdata="urn:schemas-microsoft-com:xml-msdata" xmlns:diffgr="urn:schemas-microsoft-com:xml-diffgram-v1">
+                <Ruonia xmlns="">
+                    <ro diffgr:id="ro1" msdata:rowOrder="0">
+                        <D0>2016-03-14T00:00:00+03:00</D0>
+                        <ruo>11.0700</ruo>
+                        <vol>150.4600</vol>
+                    </ro>
+                    <ro diffgr:id="ro2" msdata:rowOrder="1">
+                        <D0>2016-03-15T00:00:00+03:00</D0>
+                        <ruo>11.1000</ruo>
+                        <vol>185.8700</vol>
+                    </ro>
+                </Ruonia>
+                </diffgr:diffgram>
+            </RuoniaResult>
+        </RuoniaResponse>
+    </soap:Body>
+</soap:Envelope>
+</body>
+</html>
+"""
